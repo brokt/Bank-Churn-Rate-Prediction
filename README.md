bc5141d36797555a44e851468f84745eb3e694b9
raj_nandu <rajnandu2711@gmail.com>
2018-05-06 01:09:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bank Churn Rate Prediction
+Predicting if a customer is likely to leave a bank. 
+
+## Input Format
+Input layer neurons(12)
+0 - France 0 or 1
+1 - Spain 0 or 1
+2 - Germany 0 or 1
+3 - CreditScore
+4 - Gender 0 -Female and 1 Male
+5 - Age
+6 - Tenure
+7 - Balance
+8 - NumOfProducts
+9 - HasCrCard
+10 - IsActiveMember
+11 - EstimatedSalary
+
+
+## Dependencies
+1) Tensorflow
+2) Keras
+3) numpy
+4) pandas
+5) sklearn
---
+++ ann.py
@@ -0,0 +1,106 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+#for country
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+#for gender
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Make the ANN
+# importing keras library
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+#initializing the ANN
+classifier = Sequential()
+
+# adding the input layer and first hidden layer with dropout
+classifier.add(Dense(output_dim=18,input_dim=12,kernel_initializer = 'uniform',activation = 'relu'))
+classifier.add(Dropout(p = 0.1))
+classifier.add(Dense(output_dim=20,input_dim=18,kernel_initializer = 'uniform',activation = 'relu'))
+classifier.add(Dropout(p = 0.2))
+classifier.add(Dense(output_dim=12,input_dim=20,kernel_initializer = 'uniform',activation = 'relu'))
+classifier.add(Dropout(p = 0.3))
+classifier.add(Dense(output_dim=6,input_dim=12, kernel_initializer = 'uniform',activation = 'relu'))
+classifier.add(Dropout(p = 0.1))
+
+#adding output layer
+classifier.add(Dense(output_dim=1, input_dim=6,kernel_initializer = 'uniform',activation = 'sigmoid'))
+
+#compiling the ANN
+classifier.compile(optimizer = 'adam',loss = 'binary_crossentropy',metrics = ['accuracy'])
+
+# Training the ANN
+classifier.fit(X_train,y_train,batch_size = 10,epochs = 100)
+
+
+def predict():
+    print("Enter country - 'spain','france' or 'germany'")
+    country = input()
+    print("Enter credit score")
+    score = float(input())
+    print("Enter the gender: 0 for female and 1 for male")
+    g = float(input())
+    print("Enter the age")
+    age = float(input())
+    print("Enter the tenure")
+    tenure = float(input())
+    print("Enter the balance")
+    balance = float(input())
+    print("Enter the number of products used by the customer")
+    numprods = float(input())
+    print("Does the user has a credit card? 1 for yes and 0 for no")
+    hascrd = float(input())
+    print("Is the customer an active Member?  1 for yes and 0 for no")
+    isactive = float(input())
+    print("Enter the estimated salary")
+    salary = float(input())
+    if country == 'france':
+        c1 = 1
+        c2 = 0
+        c3 = 0
+    elif country == 'spain':
+        c1 = 0
+        c2 = 1
+        c3 = 0
+    else:
+        c1 = 0
+        c2 = 0
+        c3 = 1
+        
+    pred =  classifier.predict(sc.transform(np.array([[c1,c2,c3,score,g,age,tenure,balance,numprods,hascrd,isactive,salary]])))
+    if pred > 0.5:
+        print("The customer is likely to leave the bank")
+    else:
+        print("The customer is not likely to leave the bank")
+predict()
